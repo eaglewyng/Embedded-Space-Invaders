794a7cb8f118b47e27548c98f5b0537f4ccf1761
eaglewyng <parker.ridd@byu.net>
2014-10-29 21:07:28 -0600

    Fixed many things, but the red spaceship from el infierno still hates us Said spaceship slows down bullets and screeches loudly while crossing screen To do: downconvert higher quality sound for our use, because right now it kind of stinks

--- space_invaders/src/space_invaders.h
+++ space_invaders/src/space_invaders.h
@@ -304,6 +304,8 @@ void clearRedSpaceship();
 
 //------------sounds.c----------------//
 void deactiveRedSpaceshipOnScreenSound();
+void activeInvaderDieSound();
+void adjustVolume(int);
 
 
 #endif /* SPACE_INVADERS_H_ */
